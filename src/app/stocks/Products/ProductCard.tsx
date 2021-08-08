cf4b9f931358d751ba176312153666d50259b3b8
Bilo Lwabona <bilo@revix.com>
2021-08-08 22:36:37 +0200

    updated bulk-trade-ui

--- src/app/stocks/Products/ProductCard.tsx
+++ src/app/stocks/Products/ProductCard.tsx
@@ -2,73 +2,105 @@ import React, { useState } from 'react'
 import FAIcon from 'react-fontawesome'
 import Download from 'components/Download'
 
-const ProductCard = ({ product, isMobile }: { product: any; isMobile?: boolean }) => {
-  const [tab, setTab] = useState<string | null>(null)
+const ProductCard = ({ product, isMobile, defaultTradeType, defaultTab }: { product: any; isMobile?: boolean, defaultTradeType?: boolean, defaultTab?: boolean }) => {
+  const [tab, setTab] = useState<string | null>(defaultTab)
+  const [tradeType, setTradeType] = useState<'buy' | 'sell' | null>(defaultTradeType)
+
   const setOrDeactivateTab = (value: string) => {
     setTab(tab === value ? null : value)
   }
 
   return (
-    <div className={`product-card ${isMobile ? 'mobile' : 'desktop'}`} style={{ opacity: tab === null ? '0.6' : 1 }}>
-      {/* </div> style={{ background: `linear-gradient(to left, ${product?.color} 10%, #202020 100%)`}}> */}
-      <div className="flex-row">
-        <img src={product?.icon} alt={product?.code} style={{ width: '1.5rem', height: '1.5rem' }}/>
-        <div style={{ lineHeight: '2rem' }} className="flex-row space-between full-width">
-          <div className="flex-row">
-            <div style={{ marginRight: '1rem' }} />
-            <div>
-              {product.name}
+    <div>
+      <div className={`product-card ${isMobile ? 'mobile' : 'desktop'}`} style={{ opacity: tab === null ? 0.7 : 1 }}>
+        {/* </div> style={{ background: `linear-gradient(to left, ${product?.color} 10%, #202020 100%)`}}> */}
+        <div className="flex-row">
+          <img src={product?.icon} alt={product?.code} style={{ width: '1.5rem', height: '1.5rem' }}/>
+          <div style={{ lineHeight: '2rem' }} className="flex-row space-between full-width">
+            <div className="flex-row">
+              <div style={{ marginRight: '1rem' }} />
+              <div>
+                {product.name}
+              </div>
             </div>
-          </div>
 
-          <Download preview hasIcon iconRight src={product?.whitepaper}>
+            <Download preview hasIcon iconRight src={product?.whitepaper}>
             Paper
-          </Download>
-        </div>
-      </div>
-      {/* TABS */}
-      <div className='flex-row space-between' style={{ marginTop: '1rem' }}>
-        <div className='flex-row' onClick={() => setOrDeactivateTab('info')}>
-          <FAIcon name='info-circle' style={{ marginRight: '.5rem', fontSize: '1.5rem' }}/>
-          <div>Info</div>
-        </div>
-        <div className='flex-row' onClick={() => setOrDeactivateTab('chart')}>
-          <FAIcon name='chart-line' />
-        </div>
-        <div className='flex-row' onClick={() => setOrDeactivateTab('trade')}>
-          <div>Trade</div>
-          <FAIcon name='exchange-alt' style={{ marginLeft: '.5rem' }}/>
-        </div>
-      </div>
-      {
-        tab !== null && (
-          <>
-            <br />
-            <div className='divider horizontal' />
-            <br />
-          </>
-        )}
-      {/* ACTIVE TAB */}
-      {
-        tab === 'info' && (
-          <div className="overflow-hidden">
-            <p>{product?.summary}</p>
+            </Download>
           </div>
-        )}
-      {
-        tab === 'chart' && (
-          <div>
+        </div>
+        {/* TABS */}
+        <div className='flex-row space-between' style={{ marginTop: '1rem' }}>
+          <div className='flex-row pointer' onClick={() => setOrDeactivateTab('info')}>
+            <FAIcon name='info-circle' style={{ marginRight: '.5rem', fontSize: '1.5rem' }} className='pointer'/>
+            <div className='pointer'>Info</div>
+          </div>
+          <div className='flex-row pointer' onClick={() => setOrDeactivateTab('chart')}>
+            <FAIcon name='chart-line' className='pointer'/>
+          </div>
+          <div className='flex-row pointer' onClick={() => setOrDeactivateTab('trade')} >
+            <div className='pointer'>Trade</div>
+            <FAIcon name='exchange-alt' style={{ marginLeft: '.5rem' }} className='pointer'/>
+          </div>
+        </div>
+        {
+          !!tab && (
+            <>
+              <br />
+              <div className='divider horizontal' />
+              <br />
+            </>
+          )}
+        {/* ACTIVE TAB */}
+        {
+          tab === 'info' && (
+            <div className="overflow-hidden">
+              <p>{product?.summary}</p>
+            </div>
+          )}
+        {
+          tab === 'chart' && (
+            <div>
             Chart
-          </div>
-        )}
-      {
-        tab === 'trade' && (
-          <div>
-            Trade
-          </div>
-        )}
+            </div>
+          )}
+        {
+          tab === 'trade' && (
+            <div style={{ paddingBottom: '.5rem' }}>
+              {
+                <div className="trade-types">
+                  <div
+                    onClick={() => setTradeType('buy')}
+                    className={`item ${tradeType === 'buy' ? 'active' : ''}`}>
+                      Buy
+                  </div>
+                  <div
+                    onClick={() => setTradeType('sell')}
+                    className={`item ${tradeType === 'sell' ? 'active' : ''}`}>
+                      Sell
+                  </div>
+                </div>
+              }
+              {
+                tradeType !== null && (
+                  <>
+                    <div className="text-center" style={{ marginTop: '.5rem' }}>
+                      {tradeType === 'buy' ? 'using' : 'for'}
+                    </div>
+                    <input className="trade-input" type="number" placeholder="0.00"/>
+                  </>
+                )
+              }
+            </div>
+          )}
+      </div>
     </div>
   )
+}
+
+ProductCard.defaultProps = {
+  defaultTradeType: null,
+  defaultTab: null
 }
 
 export default ProductCard
