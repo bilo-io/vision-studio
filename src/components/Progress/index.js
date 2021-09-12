4bb9a1c25601b21c93a7a8080a4846ad66596441
Bilo Lwabona <bilo@revix.com>
2021-09-12 21:56:04 +0200

    lint fixes

--- src/components/Progress/index.js
+++ src/components/Progress/index.js
@@ -8,9 +8,9 @@ export const Progress = ({ ratio, color, items, ...rest }) => {
   // debugger
   const downloadRatio = items
     ? items.length === 0
-        ? 0
+      ? 0
       // eslint-disable-next-line no-return-assign
-        : items.map(item => item.ratio).reduce((prev, curr) => curr += prev) / items.length
+      : items.map(item => item.ratio).reduce((prev, curr) => curr += prev) / items.length
     : ratio
 
   const backgroundBarStyle = {
@@ -33,7 +33,7 @@ export const Progress = ({ ratio, color, items, ...rest }) => {
       {items?.length > 1 && (items || []).map((item, i) => <div
         key={i} style={{ marginLeft: '10%', fontSize: '0.7rem', overflow: 'hidden', textOverflow: 'ellipsis', marginBottom: '0.25rem' }}
         className='flex-row'
-                                                           >
+      >
         <Circular
           diameter={32}
           progress={item.ratio}
@@ -47,7 +47,7 @@ export const Progress = ({ ratio, color, items, ...rest }) => {
           <div style={{ width: item.ratio * 100 + `%`, height: '100%', borderRadius: '2px', backgroundColor: color, opacity: 0.8 }} />
       </div>
       */}
-                                                           </div>)}
+      </div>)}
     </div>
   )
 }
