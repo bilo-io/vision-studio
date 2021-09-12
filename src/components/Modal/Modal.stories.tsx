ebcea57c3df90f15b77042c867627fc94c8b8676
Bilo Lwabona <bilo@revix.com>
2021-09-12 19:20:28 +0200

    updated stories

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Modal } from 'components'
+import { Modal } from 'components/Modal'
 import { withAppBody, withProvider } from '../../../.storybook/utils/provider'
+import Button from 'components/Button'
 
 export default {
   title: 'Core/Modal',
@@ -10,8 +11,14 @@ export default {
 
 export function ModalStories () {
   return (
-    <div>
-      ModalStories
+    <div style={{ width: '100%', height: '100vh' }}>
+      <Modal header={'Default Modal'} isOpen onClose={() => { }}
+        footer={<div>
+          <Button color="primary">Hello</Button>
+        </div>}
+      >
+        {'Cool. Just stay in the driveway; the kill-bots are live, and I took you off the whitelist. Slow down! Uh ohhh! Somersault jump! I\'ll be with Reuben in my workshop while you guys are having another day in Phil Collins proverbial paradise.'}
+      </Modal>
     </div>
   )
 }
--- src/components/Table/Table.stories.tsx
+++ src/components/Table/Table.stories.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+/* eslint-disable react/display-name */
+import React, { useMemo } from 'react'
 import Table from '.'
 import { withAppBody, withProvider } from '../../../.storybook/utils/provider'
 
@@ -9,9 +10,76 @@ export default {
 }
 
 export function TableStories () {
+  const data = [
+    {
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john.doe@gmail.com',
+      image: 'https://www.seekpng.com/png/detail/41-410093_circled-user-icon-user-profile-icon-png.png'
+    },
+    {
+      firstName: 'Alice',
+      lastName: 'Crypt',
+      email: 'alice.crypt@gmail.com',
+      image: 'https://female-media.femalemag.com.sg/source/2016/06/H110-750x500.jpg'
+    }
+  ]
+
+  const columns = useMemo(
+    () => [
+      {
+        accessor: 'image',
+        Header: '',
+        Cell: ({ value, row }: { value: any; row: any }) => {
+          return (
+            <div>
+              <img src={value} style={{ width: '3rem', height: '3rem', borderRadius: '50%' }} />
+            </div>
+          )
+        }
+      },
+      {
+        accessor: 'firstName',
+        Header: 'First name',
+        Cell: ({ value, row }: { value: any; row: any }) => {
+          return (
+            <div>
+              {value}
+            </div>
+          )
+        }
+      },
+      {
+        accessor: 'lastName',
+        Header: 'Last name',
+        Cell: ({ value, row }: { value: any; row: any }) => {
+          return (
+            <div>
+              {value}
+            </div>
+          )
+        }
+      },
+      {
+        accessor: 'email',
+        Header: 'Email address',
+        Cell: ({ value }: { value: any; row: any }) => {
+          return (
+            <div>
+              {value}
+            </div>
+          )
+        }
+      }
+
+    ]
+    , [data])
   return (
     <div>
-      TableStories
+      <Table
+        data={data}
+        columns={columns}
+      />
     </div>
   )
 }
