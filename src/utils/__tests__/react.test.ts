90f6d97c173a94c63e9c06f30ff9d2aa675be08d
Bilo Lwabona <bilo@revix.com>
2021-09-13 03:19:52 +0200

    added react.test.ts

---
+++ src/utils/__tests__/react.test.ts
@@ -0,0 +1,8 @@
+import { useState } from '../react'
+
+describe('utils/react', () => {
+  test('useStateWithCallback', () => {
+    // const hooks = useState<string>('what')
+    expect(useState).not.toBe(null)
+  })
+})
