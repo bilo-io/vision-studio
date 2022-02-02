ab2fa6788c5ff603f86238235c82987fd92294a0
Bilo Lwabona <bilo@revix.com>
2022-02-02 23:10:55 +0200

    fix test

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -5,7 +5,7 @@ import env from 'utils/env'
 
 console.log(
   'utils/api',
-  { apiURL: `${env.apiUrl.slides}` }
+  { apiURL: `${env.apiUrl?.slides}` }
 )
 
 type ICRUDAction = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH'
@@ -23,19 +23,19 @@ const CrudAction = async (method: ICRUDAction, url: string, data?: any) => {
 }
 
 export const GETResource = async ({ appName, resource, id, query }: any) => {
-  return await CrudAction('GET', `${env.apiUrl[appName]}/${appName}/${resource}`)
+  return await CrudAction('GET', `${env?.apiUrl?.[appName]}/${appName}/${resource}`)
 }
 
 export const POSTResource = async ({ appName, resource, data }: any) => {
-  return await CrudAction('POST', `${env.apiUrl[appName]}/${appName}/${resource}`, data)
+  return await CrudAction('POST', `${env?.apiUrl?.[appName]}/${appName}/${resource}`, data)
 }
 
 export const PATCHResource = async ({ appName, resource, id, data }: any) => {
-  return await CrudAction('PATCH', `${env.apiUrl[appName]}/${appName}/${resource}/${id}`, data)
+  return await CrudAction('PATCH', `${env?.apiUrl?.[appName]}/${appName}/${resource}/${id}`, data)
 }
 
 export const DELETEResource = async ({ appName, resource, id }: any) => {
-  return await CrudAction('DELETE', `${env.apiUrl[appName]}/${appName}/${resource}/${id}`)
+  return await CrudAction('DELETE', `${env?.apiUrl?.[appName]}/${appName}/${resource}/${id}`)
 }
 
 export const handleError = (addToast: Function) => (error: any) => {
